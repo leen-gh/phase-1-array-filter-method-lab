d118c66c77e83eb4d832e4f2ba191fc89f5d0bb5
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-17 00:03:46 +0300

    assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(arry, stringy){
+    const matchingNames = arry.filter(function(name){
+        return name.toLowerCase() == stringy.toLowerCase()
+    })
+    return matchingNames
+}
+
+function fuzzyMatch(arry, str){
+
+    const n = str.length
+    const matchingNames = arry.filter(function(name){
+        return name.slice(0,n).toLowerCase() == str.toLowerCase()
+    })
+    return matchingNames
+
+}
+
+function matchName(arry, str){
+    const matchingNames = arry.filter(function (user) {
+        return user.name == str
+      });
+    return matchingNames
+}
